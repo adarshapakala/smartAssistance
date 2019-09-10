84b2aea4427371a1d4adb83c80836ba2b7661df4
adarsh <adarshapakala@gmail.com>
2019-09-10 08:53:08 +0530

    First Working - need to work on parsing

--- webScraper.py
+++ webScraper.py
@@ -1,3 +1,13 @@
 from bs4 import BeautifulSoup as bs
-from urllib.request import urlopen as myURL
+import requests
+import selenium.webdriver as wd
 
+url ="https://www.screener.in/company/LTTS/"
+headers ={"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+
+page = requests.get(url, headers=headers)
+
+soup = bs(page.content, 'html.parser')
+## print(soup.prettify())
+price = soup.find_all(attrs={'class':'four columns'})
+print(price[0])
