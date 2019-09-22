0ff4ff8648b2749ef140b1b023a3c85836a14b35
adarsh <adarshapakala@gmail.com>
2019-09-22 20:28:47 +0530

    First Working - need to work on parsing

--- webScraper.py
+++ webScraper.py
@@ -2,12 +2,26 @@ from bs4 import BeautifulSoup as bs
 import requests
 import selenium.webdriver as wd
 
-url ="https://www.screener.in/company/LTTS/"
+url ="https://www.moneycontrol.com/india/stockpricequote/banks-private-sector/yesbank/YB"
 headers ={"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
 
 page = requests.get(url, headers=headers)
 
 soup = bs(page.content, 'html.parser')
 ## print(soup.prettify())
-price = soup.find_all(attrs={'class':'four columns'})
-print(price[0])
+for lists in soup.find_all('ul', {'class':'clearfix val_listinner'}):
+    for col in soup.find_all('li', {'class':'clearfix'}):
+        for row in soup.find_all('div',{'class':'value_txtfl'}):
+            row1=str(row)
+            a=row1.find('Market Cap')
+            if a > 0:
+                val = soup.find('div',{'class':'value_txtfr'})
+                print(soup.name.string)
+
+
+
+
+    # for cell2 in row.find_all(text=True):
+    #     print(cell2)
+    # for cell in row.find(['b']):
+    #     print(cell)
