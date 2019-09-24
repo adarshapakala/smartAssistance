408ac3252385f3983e79ef21d3588fc1b9829a34
adarsh <adarshapakala@gmail.com>
2019-09-24 22:22:42 +0530

    First Working - need to work on parsing

--- webScraper.py
+++ webScraper.py
@@ -9,19 +9,11 @@ page = requests.get(url, headers=headers)
 
 soup = bs(page.content, 'html.parser')
 ## print(soup.prettify())
-for lists in soup.find_all('ul', {'class':'clearfix val_listinner'}):
-    for col in soup.find_all('li', {'class':'clearfix'}):
-        for row in soup.find_all('div',{'class':'value_txtfl'}):
-            row1=str(row)
-            a=row1.find('Market Cap')
-            if a > 0:
-                val = soup.find('div',{'class':'value_txtfr'})
-                print(soup.name.string)
+a = soup.find_all('div', {'class':'value_txtfl'})
+b = soup.find_all('div', {'class':'value_txtfl'})
+for row in a:
+    print(row.string)
+print('#########################################################################################################')
+print(a)
 
 
-
-
-    # for cell2 in row.find_all(text=True):
-    #     print(cell2)
-    # for cell in row.find(['b']):
-    #     print(cell)
